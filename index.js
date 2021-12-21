07d23479be9645500529553d84f06c34579307da
Valtteri Tuominen <valtteri.tuominen@aalto.fi>
2021-12-21 21:08:06 +0200

    readded fronted

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const Person = require('./models/person')
 
 app.use(cors())
-//app.use(express.static('build'))
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
